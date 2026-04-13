7896c2e19c7c6141c41dbee8eb6e6732e482d17c
BurntToasters <61037367+BurntToasters@users.noreply.github.com>
2026-04-13 15:27:11 -0700

    add fiveserver support and update headers

--- global/pwa.js
+++ global/pwa.js
@@ -1,6 +1,19 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').catch(function() {
+        navigator.serviceWorker.getRegistrations().then(function(registrations) {
+            return Promise.all(registrations.map(function(registration) {
+                return registration.unregister();
+            }));
+        }).catch(function() {
         });
+
+        if ('caches' in window) {
+            caches.keys().then(function(keys) {
+                return Promise.all(keys.map(function(key) {
+                    return caches.delete(key);
+                }));
+            }).catch(function() {
+            });
+        }
     });
-}
+} 
